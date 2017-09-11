1538670e3bcec94bd418b2d607c4b955f40578ea
Dayvson <dayvson.nascimento@ccc.ufcg.edu.br>
2017-09-11 13:19:30 -0300

    initial commit

---
+++ Sample/test.als
@@ -0,0 +1,18 @@
+module banco
+
+some sig Banco{
+	contas: some Conta
+
+}
+
+abstract sig Conta{}
+
+sig ContaCorrente, ContaPoupanca extends Conta{}
+
+fact bankDetails{
+	all c:Conta | one  c.~contas
+}
+
+pred show[]{}
+
+run show for 5
---
+++ src/alloyToXML/ModuleFromFile.java
@@ -0,0 +1,47 @@
+package alloyToXML;
+
+import edu.mit.csail.sdg.alloy4.A4Reporter;
+import edu.mit.csail.sdg.alloy4.Err;
+import edu.mit.csail.sdg.alloy4compiler.ast.Command;
+import edu.mit.csail.sdg.alloy4compiler.parser.CompModule;
+import edu.mit.csail.sdg.alloy4compiler.parser.CompUtil;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Options;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
+import edu.mit.csail.sdg.alloy4compiler.translator.TranslateAlloyToKodkod;
+
+public class ModuleFromFile {
+
+	public static void loadModule(String fileName) {
+
+		A4Reporter rep = new A4Reporter();
+		
+		try {
+			CompModule loaded = CompUtil.parseEverything_fromFile(rep, null, fileName);
+			printSolutions(rep, loaded);
+		} catch (Err e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void printSolutions(A4Reporter rep, CompModule loaded) {
+
+		A4Options options = new A4Options();
+		options.solver = A4Options.SatSolver.SAT4J;
+		
+		try {
+			for (Command command : loaded.getAllCommands()) {
+				System.out.println("============ Command: " + command + ": ============");
+				A4Solution ans = TranslateAlloyToKodkod.execute_commandFromBook(rep, loaded.getAllReachableSigs(),
+						command, options);
+				System.out.println(ans);
+			}
+		} catch (Err e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		loadModule("Sample/test.als");
+	}
+}
